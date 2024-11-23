6bcac4475f03ce179afb67e8d9be0b4c741d9837
LautaroDevelopers <lautaroledesma2021@gmail.com>
2024-11-23 13:11:35 -0300

    canales funcionando y renderizando

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const channelController = require("../controllers/channelController");
+const conexion = require("../database/db");
 
 router.get("/", (req, res) => {
   res.render("layouts/dashboard", {
@@ -26,12 +26,43 @@ router.get("/tv", (req, res) => {
   });
 });
 
-router.get("/player/:id", channelController.playChannel, (req, res) => {
-  res.render("layouts/dashboard", {
-    user: req.user,
-    title: "Player",
-    body: "../dashboard/player",
+router.get("/add-channel", (req, res) => {
+  res.render("dashboard/add-channel"); // Crea esta vista.
+});
+
+router.post("/add-channel", (req, res) => {
+  const { name, logo_url, video_url, category } = req.body;
+  conexion.query(
+    "INSERT INTO channels (name, logo_url, video_url, category) VALUES (?, ?, ?, ?)",
+    [name, logo_url, video_url, category],
+    (err) => {
+      if (err) throw err;
+      res.redirect("/channels"); // Redirige a la lista de canales.
+    }
+  );
+});
+
+router.get("/channels", (req, res) => {
+  conexion.query("SELECT * FROM channels", (err, results) => {
+    if (err) throw err;
+    res.render("dashboard/channels", { channels: results });
   });
+});
+
+router.get("/channel/:id", (req, res) => {
+  const channelId = req.params.id;
+  conexion.query(
+    "SELECT * FROM channels WHERE id = ?",
+    [channelId],
+    (err, results) => {
+      if (err) throw err;
+      if (results.length > 0) {
+        res.render("dashboard/player", { channel: results[0] });
+      } else {
+        res.redirect("/channels");
+      }
+    }
+  );
 });
 
 router.get("/profile", (req, res) => {
