4bdb9d9290ffc771c9f8a1d9754e0720364a3560
LautaroDevelopers <lautaroledesma2021@gmail.com>
2024-11-26 04:36:37 -0300

    radios funcionando, trabajando en home

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const channelController = require("../controllers/channelController");
+const conexion = require("../database/db");
 
 router.get("/", (req, res) => {
   res.render("layouts/dashboard", {
@@ -10,12 +11,50 @@ router.get("/", (req, res) => {
   });
 });
 
+// Ruta para mostrar radios
 router.get("/fm", (req, res) => {
-  res.render("layouts/dashboard", {
-    user: req.user,
-    title: "FM",
-    body: "../dashboard/fm",
+  conexion.query("SELECT * FROM radios", (err, results) => {
+    if (err) throw err;
+    res.render("layouts/dashboard", {
+      radios: results,
+      user: req.user,
+      title: "FM",
+      body: "../dashboard/fm",
+    });
   });
+});
+
+// Ruta para agregar un radio
+router.get("/add-fm", (req, res) => {
+  res.render("dashboard/add-fm");
+});
+
+router.post("/add-fm", (req, res) => {
+  const { name, logo_url, stream_url, category, location } = req.body;
+  conexion.query(
+    "INSERT INTO radios (name, logo_url, stream_url, category, location) VALUES (?, ?, ?, ?, ?)",
+    [name, logo_url, stream_url, category, location],
+    (err) => {
+      if (err) throw err;
+      res.redirect("/fm");
+    }
+  );
+});
+
+router.get("/fm/:id", (req, res) => {
+  const radioId = req.params.id;
+  conexion.query(
+    "SELECT * FROM radios WHERE id = ?",
+    [radioId],
+    (err, results) => {
+      if (err) throw err;
+      if (results.length > 0) {
+        res.render("dashboard/fm-player", { radio: results[0] });
+      } else {
+        res.redirect("/fm");
+      }
+    }
+  );
 });
 
 // Renderizamos los canales en la vista TV
